7f61a4478f017dadb07ad75c7db942a9b32355ef
SkadiTheWolf <147843853+SkadiTheWolf@users.noreply.github.com>
2024-03-29 22:23:50 +0100

    cleanup

--- sandpiles.java
+++ sandpiles.java
@@ -7,7 +7,6 @@
   */
 
 import java.util.*;
-import java.util.Scanner;
 import java.util.stream.*;
 
 
@@ -23,68 +22,65 @@ public class sandpiles {
   public static int length = 9;
   
   public static void main(String[] args) {
-        
-        define_x(); 
-        define_y();
-        addition();
-    
-        int topple = 4;
-        boolean doesContain = IntStream.of(sandpiles.z).anyMatch(x -> x >= 4);
-    
+
+      define_x();
+    define_y();
+    addition();
+
+    boolean doesContain = IntStream.of(sandpiles.z).anyMatch(x -> x >= 4);
+
     if (doesContain == true) {
-      for (int i = 0;i <= sandpiles.length; i++ ) {
-         finalisation();
-        } // end of for
+      for (int i = 0; i <= sandpiles.length; i++) {
+        finalisation();
+      } // end of for
     } // end of if    
-     System.out.println();
+
+    System.out.println();
     System.out.println("Your sum is:");
-         System.out.println("("+ z[0] +")  (" + z[1] + ")  (" + z[2] + ")");
-         System.out.println("("+ z[3] +")  (" + z[4] + ")  (" + z[5] + ")");
-         System.out.println("("+ z[6] +")  (" + z[7] + ")  (" + z[8] + ")");
-  }   //end of main
-  
+    System.out.println("(" + z[0] + ")  (" + z[1] + ")  (" + z[2] + ")");
+    System.out.println("(" + z[3] + ")  (" + z[4] + ")  (" + z[5] + ")");
+    System.out.println("(" + z[6] + ")  (" + z[7] + ")  (" + z[8] + ")");
+
+       } //end of main
+ 
+@SuppressWarnings("resource")
 public static void define_x() {
   
         Scanner sc = new Scanner(System.in);
- 
-        // Take the array size from the user
         int arr_size = 9;
  
-        // Initialize the array's
-        // size using user input
         sandpiles.x = new int[arr_size];
  
-        // Take user elements for the array
+        // user input
         System.out.println(
-            "Enter the elements of the Sandpile: ");
+            "Enter the elements of the first Sandpile: ");
         for (int i = 0; i < arr_size; i++) {
             if (sc.hasNextInt()) {
               
                 sandpiles.x[i] = sc.nextInt();
               
             } //end of if
-        }  //end of for
+          } //end of for
+        
+          //sc_x.close(); //close sc
 
        System.out.println("Your first sandpile is:");
     
        System.out.println("("+ x[0] +")  (" + x[1] + ")  (" + x[2] + ")");
        System.out.println("("+ x[3] +")  (" + x[4] + ")  (" + x[5] + ")");
-       System.out.println("("+ x[6] +")  (" + x[7] + ")  (" + x[8] + ")");
+       System.out.println("(" + x[6] + ")  (" + x[7] + ")  (" + x[8] + ")");
+       
+       
       
   } //end of define_x()
   
   public static void define_y() {
   
         Scanner sc = new Scanner(System.in);
- 
-        // Take the array size from the user
         int arr_size = 9;
- 
-        // Initialize the array's
-        // size using user input
+
+        //user input
         sandpiles.y = new int[arr_size];
- 
-        // Take user elements for the array
         System.out.println("Enter the elements of the second sandpile: ");
         for (int i = 0; i < arr_size; i++) {
             if (sc.hasNextInt()) {
@@ -92,7 +88,9 @@ public static void define_x() {
                 sandpiles.y[i] = sc.nextInt();
               
             } //end of if
-        }  // end of for
+          } // end of for
+        
+          sc.close(); //close sc
     
        System.out.println("Your second sandpile is:");
     
